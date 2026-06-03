4c3b3111923b6d7c0b17aecb75a748f451087c2e
tavarespereira555 <tavarespereira555@gmail.com>
2026-06-03 11:29:12 -0300

    subindo projetos- 11:28

---
+++ treinando logica p prova/Prova-lista.py
@@ -0,0 +1,13 @@
+import random
+lista= []
+nova_lista= []
+for i in range(4,40 + 2,2):
+    #num= random.randint(1, 100)
+
+    lista.append(i)
+
+#novo_valor= 5
+#lista[6] = novo_valor
+#nova_lista= sorted(lista, reverse= False)
+print(lista)
+print(nova_lista)
